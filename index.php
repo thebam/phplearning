eba899f9203db01eff775676b53d2a0cac9d2773
thebam <jason.j.saunders@gmail.com>
2016-02-26 16:32:22 -0500

    Added some php basics

--- index.php
+++ index.php
@@ -11,20 +11,58 @@ echo is_string($name);
 // echo is_string($age);
 // 
 // echo $age;
-// echo GENDER;
+echo GENDER;
+$strAge = (string)$age;
+echo $strAge;
 
 doSomething($name,returnSomething());
-doSomething($name,returnSomething($age ));
+doSomething($name,returnSomething($age));
 
 echo $alive;
 setGlobal();
 echo $alive;
 
+$uppercase = 'this began lowercase';
+$lowercase = 'THIS BEGAN UPPERCASE';
+
+$uppercase = strtoupper($uppercase);
+$lowercase = strtolower($lowercase);
+
+echo $uppercase;
+echo $lowercase;
+
+$authors = array('Jason','La Shan', 'Elana');
+array_push($authors, 'Janet');
+//this is preferred because it will create an array if it doesn't exists
+$authors[] = 'Janet';
+sort($authors);
+
+print_r($authors);
+foreach($authors as $author){
+    echo($author);
+}
+
+
+$members = array('dad'=>'Jason', 'mom'=>'La Shan','baby'=>'Elana'); 
+if (array_key_exists('dad',$members)){ 
+    echo $members['dad'];
+}
+asort($members);
+print_r($members);
+
+ksort($members);
+print_r($members);
+
+foreach($members as $key => $member){ 
+    echo($key); 
+    echo($member); 
+}
+
+
+
 
 function doSomething($myName, $myAge){
-    echo $myName ;
-    echo "\n";
-    echo ($myAge);
+    echo "$myName will be ". $myAge ." in 5 years.";
     echo "\n";
 }
 
