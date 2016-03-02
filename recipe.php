1589c44a931c561aea0f5f3b887aabcaf79a1b74
thebam <jason.j.saunders@gmail.com>
2016-03-02 16:59:23 -0500

    Refine CRUD in recipe class.

--- recipe.php
+++ recipe.php
@@ -10,18 +10,24 @@ class Recipe
     private $connection;
     private $recipes;
     
-    
     public function addRecipe($tempTitle, $tempMainIngredient,$tempUrl){
-        $this->connection = openConnection();
-        
-        $query = 'INSERT INTO recipe (title, mainIngredient,url) VALUES (?,?,?)';
-        $recipes = $this->connection->prepare($query);
-        $recipes->bind_param('sss',$tempTitle, $tempMainIngredient,$tempUrl);
-        $recipes->execute();
-        //echo "new id is ".$connection->insert_id;
-        // 
-        $recipes->close();
-        $this->connection->close();
+        if($tempTitle!==NULL &&  $tempMainIngredient !== NULL && $tempUrl !== NULL)
+        {
+            $this->connection = openConnection();
+            $query = 'INSERT INTO recipe (title, mainIngredient,url) VALUES (?,?,?)';
+            $recipes = $this->connection->prepare($query);
+            $recipes->bind_param('sss',$tempTitle, $tempMainIngredient,$tempUrl);
+            $recipes->execute(); 
+            $recipes->close();
+            
+            if($this->connection->insert_id > 0){
+                $this->connection->close();
+                header('Location index.php');   
+            }else{
+                $this->connection->close();
+                header('Location error.php');
+            }
+        }
     }
     
     public function allRecipes(){
@@ -34,6 +40,26 @@ class Recipe
     public function closeRecipes(){
          $this->recipes->close();
           $this->connection->close();
+    }
+    
+    public function getRecipeById(){}
+    
+    public function getRecipeByName($recipeName){
+        $connection = openConnection();
+        $query = 'SELECT TOP 1 * FROM recipe WHERE title = ?';
+        $receipe = $connection->query($query);
+        if ($recipe->num_rows>0) {
+            while($meal = $recipe->fetch_assoc()){
+                $this->id = $meal['id'];
+$this->title = $meal['title'];
+$this->mainIngredient = $meal['mainIngredient'];
+$this->url = $meal['url'];
+$this->dateModified = $meal['dateModified'];
+            }
+        }
+        $recipe->close();
+        $connection->close();
+        return $this;
     }
 }
 ?>
